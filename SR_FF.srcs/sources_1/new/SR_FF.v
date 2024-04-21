a06b9176e4722cae81a37176f3f313f6ce62ea28
Rajithxx <148357145+Rajithxx@users.noreply.github.com>
2024-04-21 15:59:48 +0530

    212221060222 SR_FF.v

--- SR_FF.srcs/sources_1/new/SR_FF.v
+++ SR_FF.srcs/sources_1/new/SR_FF.v
@@ -1,52 +1,20 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 10.01.2024 10:25:08
-// Design Name: 
-// Module Name: SR_FF
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-module SR_FF(q, qb,s,r,clock,reset);
-    input s,r,clock,reset;
-    output reg q, qb;
-always @ (posedge (clock))
-    begin 
-        if (!reset)
-            begin
-               q <= q;
-               qb <=qb;
-            end   
-else
-            begin
-                  //Write the logic for SR Flip Fliop using if-else construct
-           
-           
-           
-           
-           
-           
-           
-           
-           
-           
-           
-           
-           
-           
-           
-           
-            end
-end  
+module SR_flipflop (
+  input clk, rst_n,
+  input s,r,
+  output reg q,
+  output q_bar
+  );
+  
+  always@(posedge clk) begin 
+    if(!rst_n) q <= 0;
+    else begin
+      case({s,r})
+        2'b00: q <= q;
+        2'b01: q <= 1'b0;
+        2'b10: q <= 1'b1;
+        2'b11: q <= 1'bx;
+      endcase
+    end
+  end
+  assign q_bar = ~q;
 endmodule
